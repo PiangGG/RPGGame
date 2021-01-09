af64a5ffb0709f79836088e6740bbd98052b9347
PiangGG <piangpiangpiang@outlook.com>
2021-01-09 13:07:50 +0800

    初始化(2)

---
+++ Main/Intermediate/Build/Win64/UE4Editor/Inc/Main/Main.init.gen.cpp
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMain_init() {}
+	UPackage* Z_Construct_UPackage__Script_Main()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			static const UE4CodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Main",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xBD42E541,
+				0xC3FB8A20,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
+		}
+		return ReturnPackage;
+	}
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
